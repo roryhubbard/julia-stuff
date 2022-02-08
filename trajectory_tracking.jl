b3c2c0d488cb6db9ef4dbe34cf2846623f03d55f
roryhubbard <calvin3h@gmail.com>
2022-02-08 15:43:24 +0100

    checkpoint

--- trajectory_tracking.jl
+++ trajectory_tracking.jl
@@ -6,7 +6,18 @@ using .Utils
 export differential_flatness_trajectory
 
 
-function polynomial_trajectory(polynomial_order, p₀, p₁, v₀, v₁, θ₀, θ₁, t, is_x=false)
+function add_boundary_constraints(problem, x, p₀, p₁, v₀, v₁, a₀, a₁, t)
+  problem.constraints += [
+    evaluate_polynomial(0, x, 0) == p₀,
+    evaluate_polynomial(0, x, 1) == v₀,
+    #evaluate_polynomial(0, x, 2) == a₀,
+    evaluate_polynomial(t, x, 0) == p₁,
+    evaluate_polynomial(t, x, 1) == v₁,
+    evaluate_polynomial(t, x, 2) == a₁,
+  ]
+end
+
+function polynomial_trajectory(polynomial_order, p₀, p₁, v₀, v₁, a₀, a₁, θ₀, θ₁, t)
   if polynomial_order > 5
     error("polynomial order > 5")
   end
@@ -19,48 +30,71 @@ function polynomial_trajectory(polynomial_order, p₀, p₁, v₀, v₁, θ₀, 
     0 0 0 120t^3  360t^4 720t^5;
   ]
   po = polynomial_order
-  x = Variable(po+1)
-  objective = quadform(x, P[1:po+1, 1:po+1])
-  constraints = [
-    evaluate_polynomial(0, x, 0, po) == p₀,
-    evaluate_polynomial(0, x, 1, po) == v₀,
-    evaluate_polynomial(t, x, 0, po) == p₁,
-    evaluate_polynomial(t, x, 1, po) == v₁,
+  x_poly = Variable(po+1)
+  y_poly = Variable(po+1)
+  objective = quadform(x_poly, P[1:po+1, 1:po+1]) + quadform(y_poly, P[1:po+1, 1:po+1])
+  problem = minimize(objective)
+
+  add_boundary_constraints(problem, x_poly, p₀[1], p₁[1], v₀[1], v₁[1], a₀[1], a₁[1], t)
+  add_boundary_constraints(problem, y_poly, p₀[2], p₁[2], v₀[2], v₁[2], a₀[2], a₁[2], t)
+  problem.constraints += [
+    evaluate_polynomial(t/2, x_poly, 0) == 1,
   ]
-  if is_x
-    constraints += [
-      evaluate_polynomial(0, x, 1, po) == v₀*cos(θ₀),
-      evaluate_polynomial(t, x, 1, po) == v₁*cos(θ₁),
-    ]
-  else
-    constraints += [
-      evaluate_polynomial(0, x, 1, po) == v₀*sin(θ₀),
-      evaluate_polynomial(t, x, 1, po) == v₁*sin(θ₁),
-    ]
-  end
-  problem = minimize(objective, constraints)
+
   solve!(problem, SCS.Optimizer)
-  evaluate(x)
+  evaluate(x_poly), evaluate(y_poly)
+end
+
+
+function recover_θ(t, x_poly, y_poly)
+  x_dot = evaluate_polynomial(t, x_poly, 1)
+  y_dot = evaluate_polynomial(t, y_poly, 1)
+  atan(y_dot, x_dot)
+end
+
+
+function recover_v(t, x_poly, y_poly)
+  x_dot = evaluate_polynomial(t, x_poly, 1)
+  y_dot = evaluate_polynomial(t, y_poly, 1)
+  √(x_dot^2 + y_dot^2)
+end
+
+
+function recover_w(t, x_poly, y_poly)
+  x_dot = evaluate_polynomial(t, x_poly, 1)
+  y_dot = evaluate_polynomial(t, y_poly, 1)
+  x_ddot = evaluate_polynomial(t, x_poly, 2)
+  y_ddot = evaluate_polynomial(t, y_poly, 2)
+  (y_ddot * x_dot - x_ddot * y_dot) / (x_dot^2 + y_dot^2)
 end
 
 
 function differential_flatness_trajectory()
-  poly_order = 3
-  x_poly = polynomial_trajectory(poly_order, 0, 0, 0, 0, 0, 0, 1, true)
-  y_poly = polynomial_trajectory(poly_order, 0, 1, 0, 0, 0, 0, 1, false)
+  poly_order = 5
+  p₀ = [0. 0.]
+  p₁ = [0. 1.]
+  v₀ = [0. 0.]
+  v₁ = [0. 0.]
+  a₀ = [0. 0.]
+  a₁ = [0. 0.]
+  θ₀ = 0.
+  θ₁ = 0.
+  t = 1.
+  x_poly, y_poly = polynomial_trajectory(poly_order, p₀, p₁, v₀, v₁, a₀, a₁, θ₀, θ₁, t)
   x = Vector{Float64}()
   y = Vector{Float64}()
   θ = Vector{Float64}()
   for t in LinRange(0, 1, 100)
-    x_evaluated = evaluate_polynomial(t, x_poly, 0, poly_order)
-    y_evaluated = evaluate_polynomial(t, y_poly, 0, poly_order)
+    x_evaluated = evaluate_polynomial(t, x_poly, 0)
+    y_evaluated = evaluate_polynomial(t, y_poly, 0)
+    θ_evaluated = recover_θ(t, x_poly, y_poly)
     push!(x, x_evaluated)
     push!(y, y_evaluated)
+    push!(θ, θ_evaluated)
   end
-  plot(x, y, aspect_ratio=:equal)
-  #p1 = plot(x, y, aspect_ratio=:equal)
-  #p2 = plot(θ)
-  #display(plot(p1, p2, layout=(1, 2)))
+  p1 = plot(x, y, aspect_ratio=:equal)
+  p2 = plot(θ)
+  display(plot(p1, p2, layout=(1, 2)))
 end
 
 
